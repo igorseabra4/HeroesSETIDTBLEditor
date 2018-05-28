cb1662744a1c6351c4dd7b74cadcfc2949d54cbc
igorseabra4 <igorseabra@gmail.com>
2018-05-28 20:33:01 -0300

    syncing with local

---
+++ HeroesSETIDTBLEditor/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace HeroesSETIDTBLEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.CheckedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.ObjectNameComboBox = new System.Windows.Forms.ComboBox();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(396, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.aboutToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Enabled = false;
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Enabled = false;
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As...";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 436);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(396, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(81, 17);
+            this.toolStripStatusLabel1.Text = "No file loaded";
+            // 
+            // CheckedListBox1
+            // 
+            this.CheckedListBox1.FormattingEnabled = true;
+            this.CheckedListBox1.Location = new System.Drawing.Point(12, 54);
+            this.CheckedListBox1.Name = "CheckedListBox1";
+            this.CheckedListBox1.Size = new System.Drawing.Size(372, 379);
+            this.CheckedListBox1.TabIndex = 6;
+            this.CheckedListBox1.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.CheckedListBox1_ItemCheck);
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(12, 30);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(41, 13);
+            this.Label1.TabIndex = 5;
+            this.Label1.Text = "Object:";
+            // 
+            // ObjectNameComboBox
+            // 
+            this.ObjectNameComboBox.FormattingEnabled = true;
+            this.ObjectNameComboBox.Location = new System.Drawing.Point(59, 27);
+            this.ObjectNameComboBox.Name = "ObjectNameComboBox";
+            this.ObjectNameComboBox.Size = new System.Drawing.Size(325, 21);
+            this.ObjectNameComboBox.TabIndex = 4;
+            this.ObjectNameComboBox.SelectedIndexChanged += new System.EventHandler(this.ObjectNameComboBox_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 458);
+            this.Controls.Add(this.CheckedListBox1);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.ObjectNameComboBox);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Heroes SET ID Table Editor";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        internal System.Windows.Forms.CheckedListBox CheckedListBox1;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.ComboBox ObjectNameComboBox;
+    }
+}
+
---
+++ HeroesSETIDTBLEditor/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.IO;
+
+namespace HeroesSETIDTBLEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            if (File.Exists("Resources\\ObjectList.ini") & File.Exists("Resources\\StageList.ini"))
+            {
+                ReadObjectListData("Resources\\ObjectList.ini");
+                ReadStageListData("Resources\\StageList.ini");
+            }
+            else
+            {
+                MessageBox.Show("Error loading external files.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Close();
+            }
+
+            ProgramIsChangingStuff = false;
+        }
+
+        public class ObjectEntry
+        {
+            public byte list;
+            public byte type;
+            public string name;
+        }
+
+        List<ObjectEntry> ObjectEntryList = new List<ObjectEntry>();
+
+        private void ReadObjectListData(string FileName)
+        {
+            string[] ObjectListFile = File.ReadAllLines(FileName);
+
+            byte List = 0;
+            byte Type = 0;
+            string Name = "";
+            string DebugName = "";
+
+            foreach (string i in ObjectListFile)
+            {
+                if (i.StartsWith("["))
+                {
+                    List = Convert.ToByte(i.Substring(1, 2), 16);
+                    Type = Convert.ToByte(i.Substring(5, 2), 16);
+                }
+                else if (i.StartsWith("Object="))
+                    Name = i.Split('=')[1];
+                else if (i.StartsWith("Debug="))
+                    DebugName = i.Split('=')[1];
+                else if (i.StartsWith("EndOfFile"))
+                {
+                    ObjectEntryList.Add(new ObjectEntry()
+                    {
+                        list = List,
+                        type = Type,
+                        name = Name != "" ? Name : DebugName != "" ? DebugName : "Unknown/Unused"
+                    });
+                    break;
+                }
+                else if (i.Length == 0)
+                {
+                    ObjectEntryList.Add(new ObjectEntry()
+                    {
+                        list = List,
+                        type = Type,
+                        name = Name != "" ? Name : DebugName != "" ? DebugName : "Unknown/Unused"
+                    });
+                    List = 0;
+                    Type = 0;
+                    Name = "";
+                    DebugName = "";
+                }
+            }
+        }
+        
+        private void ReadStageListData(string FileName)
+        {
+            string[] StageListFile = File.ReadAllLines(FileName);
+
+            foreach (string i in StageListFile)
+            {
+                CheckedListBox1.Items.Add(i);
+            }
+        }
+
+        bool ProgramIsChangingStuff = false;
+
+        OpenFileDialog OpenTableFile = new OpenFileDialog();
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenTableFile.Filter = ".bin files|*.bin";
+            if (OpenTableFile.ShowDialog() == DialogResult.OK)
+                LoadTableFile();
+        }
+
+        SaveFileDialog SaveTableFile = new SaveFileDialog();
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (OpenTableFile.FileName != null & OpenTableFile.FileName != "")
+                SaveFile(OpenTableFile.FileName);
+            else
+            {
+                SaveTableFile.Filter = ".bin files|*.bin";
+                if (SaveTableFile.ShowDialog() == DialogResult.OK)
+                {
+                    OpenTableFile.FileName = SaveTableFile.FileName;
+                    SaveFile(OpenTableFile.FileName);
+                }
+            }
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveTableFile.Filter = ".bin files|*.bin";
+            SaveTableFile.FileName = OpenTableFile.FileName;
+            if (SaveTableFile.ShowDialog() == DialogResult.OK)
+            {
+                OpenTableFile.FileName = SaveTableFile.FileName;
+                SaveFile(OpenTableFile.FileName);
+            }
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Heroes SET ID Table Editor release 4 by igorseabra4", "About");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+                
+        public class TableEntry
+        {
+            public ObjectEntry Object = new ObjectEntry();
+            public byte[] Values;
+
+            public TableEntry()
+            {
+                Values = new byte[12];
+            }
+
+            public override string ToString()
+            {
+                return "[" + Object.list.ToString("X2") + "] [" + Object.type.ToString("X2") + "] " + Object.name;
+            }
+        }
+
+        private void LoadTableFile()
+        {
+            BinaryReader TableReader = new BinaryReader(new FileStream(OpenTableFile.FileName, FileMode.Open));
+
+            ObjectNameComboBox.Items.Clear();
+            byte[] FileString = TableReader.ReadBytes(20 * 512);
+            TableReader.Close();
+            
+            for (int i = 0; i < 512; i++)
+            {
+                TableEntry TemporaryEntry = new TableEntry();
+
+                TemporaryEntry.Values[0] = FileString[i * 20 + 3];
+                TemporaryEntry.Values[1] = FileString[i * 20 + 2];
+                TemporaryEntry.Values[2] = FileString[i * 20 + 1];
+                TemporaryEntry.Values[3] = FileString[i * 20 + 0];
+                TemporaryEntry.Values[4] = FileString[i * 20 + 7];
+                TemporaryEntry.Values[5] = FileString[i * 20 + 6];
+                TemporaryEntry.Values[6] = FileString[i * 20 + 5];
+                TemporaryEntry.Values[7] = FileString[i * 20 + 4];
+                TemporaryEntry.Values[8] = FileString[i * 20 + 11];
+                TemporaryEntry.Values[9] = FileString[i * 20 + 10];
+                TemporaryEntry.Values[10] = FileString[i * 20 + 9];
+                TemporaryEntry.Values[11] = FileString[i * 20 + 8];
+
+                TemporaryEntry.Object.list = FileString[i * 20 + 18];
+                TemporaryEntry.Object.type = FileString[i * 20 + 19];
+
+                foreach (ObjectEntry j in ObjectEntryList)
+                {
+                    if (j.list == TemporaryEntry.Object.list & j.type == TemporaryEntry.Object.type)
+                    {
+                        TemporaryEntry.Object = j;
+                    }
+                }
+
+                ObjectNameComboBox.Items.Add(TemporaryEntry);
+            }
+
+            saveToolStripMenuItem.Enabled = true;
+            saveAsToolStripMenuItem.Enabled = true;
+            toolStripStatusLabel1.Text = "Loaded " + OpenTableFile.FileName;
+        }
+
+        private void SaveFile(string fileName)
+        {
+            BinaryWriter LayoutFileWriter = new BinaryWriter(new FileStream(fileName, FileMode.Create));
+
+            LayoutFileWriter.BaseStream.SetLength(20 * 512);
+
+            foreach (TableEntry i in ObjectNameComboBox.Items)
+            {
+                LayoutFileWriter.Write(i.Values[3]);
+                LayoutFileWriter.Write(i.Values[2]);
+                LayoutFileWriter.Write(i.Values[1]);
+                LayoutFileWriter.Write(i.Values[0]);
+                LayoutFileWriter.Write(i.Values[7]);
+                LayoutFileWriter.Write(i.Values[6]);
+                LayoutFileWriter.Write(i.Values[5]);
+                LayoutFileWriter.Write(i.Values[4]);
+                LayoutFileWriter.Write(i.Values[11]);
+                LayoutFileWriter.Write(i.Values[10]);
+                LayoutFileWriter.Write(i.Values[9]);
+                LayoutFileWriter.Write(i.Values[8]);
+                LayoutFileWriter.Write(new byte[6]);
+                LayoutFileWriter.Write(i.Object.list);
+                LayoutFileWriter.Write(i.Object.type);
+            }
+
+            LayoutFileWriter.Close();
+            toolStripStatusLabel1.Text = "Loaded " + OpenTableFile.FileName;
+        }
+
+        byte[] Sel = { 1, 2, 4, 8, 16, 32, 64, 128 };
+
+        private void ObjectNameComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ProgramIsChangingStuff = true;
+
+            for (int j = 0; j < 12; j++)
+                for (int i = 0; i < 8; i++)
+                    if (((ObjectNameComboBox.SelectedItem as TableEntry).Values[j] & Sel[i]) == Sel[i])
+                        CheckedListBox1.SetItemCheckState(j * 8 + i, CheckState.Checked);
+                    else
+                        CheckedListBox1.SetItemCheckState(j * 8 + i, CheckState.Unchecked);
+
+            ProgramIsChangingStuff = false;
+        }
+        
+        private void CheckedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            if (!ProgramIsChangingStuff & ObjectNameComboBox.SelectedIndex > -1)
+            {
+                if (e.NewValue == CheckState.Checked)
+                    (ObjectNameComboBox.SelectedItem as TableEntry).Values[e.Index / 8] = (byte)((ObjectNameComboBox.SelectedItem as TableEntry).Values[e.Index / 8] | Sel[e.Index % 8]);
+                else if (e.NewValue == CheckState.Unchecked)
+                    (ObjectNameComboBox.SelectedItem as TableEntry).Values[e.Index / 8] = (byte)((ObjectNameComboBox.SelectedItem as TableEntry).Values[e.Index / 8] ^ Sel[e.Index % 8]);
+            }
+        }
+    }
+}
